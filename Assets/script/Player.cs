c390901bc0cbd15dfdb5a9c5e1c6b526eb36c090
GanXiKang <gan0612xk@hotmail.com>
2022-10-22 23:16:41 +0800

    添加角色方向控制

---
+++ Assets/script/Player.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    CharacterController cc;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 dir = new Vector3(h, 0, v);
+
+        cc.Move(dir * 5f * Time.deltaTime);
+    }
+}
